9846b3e8b922e94c323fab2d10159fea59d7e6c6
NILANJAN DEB <nilanjan172nsvian@gmail.com>
2024-07-22 01:01:20 +0530

    add register_table sql

--- _sql/register.sql
+++ _sql/register.sql
@@ -1,48 +1,53 @@
+CREATE SCHEMA iceberg.tpch_sf1 
+WITH (location= 'gs://trino-hive-demo/iceberg/tpch/sf1');
+
+use iceberg.tpch_sf1;
+
 CALL iceberg.system.register_table
 (
-  schema_name => 'tpch', 
+  schema_name => 'tpch_sf1', 
   table_name => 'customer', 
-  table_location => 'gs://trino-hive-demo/iceberg/tpch/customer'
+  table_location => 'gs://trino-hive-demo/iceberg/tpch/sf1/customer'
 );
 CALL iceberg.system.register_table
 (
-  schema_name => 'tpch', 
+  schema_name => 'tpch_sf1', 
   table_name => 'lineitem', 
-  table_location => 'gs://trino-hive-demo/iceberg/tpch/lineitem'
+  table_location => 'gs://trino-hive-demo/iceberg/tpch/sf1/lineitem'
 );
 CALL iceberg.system.register_table
 (
-  schema_name => 'tpch', 
+  schema_name => 'tpch_sf1', 
   table_name => 'nation', 
-  table_location => 'gs://trino-hive-demo/iceberg/tpch/nation'
+  table_location => 'gs://trino-hive-demo/iceberg/tpch/sf1/nation'
 );
 CALL iceberg.system.register_table
 (
-  schema_name => 'tpch', 
+  schema_name => 'tpch_sf1', 
   table_name => 'orders', 
-  table_location => 'gs://trino-hive-demo/iceberg/tpch/orders'
+  table_location => 'gs://trino-hive-demo/iceberg/tpch/sf1/orders'
 );
 CALL iceberg.system.register_table
 (
-  schema_name => 'tpch', 
+  schema_name => 'tpch_sf1', 
   table_name => 'part', 
-  table_location => 'gs://trino-hive-demo/iceberg/tpch/part'
+  table_location => 'gs://trino-hive-demo/iceberg/tpch/sf1/part'
 );
 CALL iceberg.system.register_table
 (
-  schema_name => 'tpch', 
+  schema_name => 'tpch_sf1', 
   table_name => 'partsupp', 
-  table_location => 'gs://trino-hive-demo/iceberg/tpch/partsupp'
+  table_location => 'gs://trino-hive-demo/iceberg/tpch/sf1/partsupp'
 );
 CALL iceberg.system.register_table
 (
-  schema_name => 'tpch', 
+  schema_name => 'tpch_sf1', 
   table_name => 'region', 
-  table_location => 'gs://trino-hive-demo/iceberg/tpch/region'
+  table_location => 'gs://trino-hive-demo/iceberg/tpch/sf1/region'
 );
 CALL iceberg.system.register_table
 (
-  schema_name => 'tpch', 
+  schema_name => 'tpch_sf1', 
   table_name => 'supplier', 
-  table_location => 'gs://trino-hive-demo/iceberg/tpch/supplier'
+  table_location => 'gs://trino-hive-demo/iceberg/tpch/sf1/supplier'
 );
