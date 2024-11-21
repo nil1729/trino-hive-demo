f1e679edd7877a33121baf0d692f4537bc89199e
Nilanjan Deb <nilanjan172nsvian@gmail.com>
2024-11-22 02:34:11 +0530

    update hive dockerfile

--- hive/docker-entrypoint.sh
+++ hive/docker-entrypoint.sh
@@ -1,23 +1,48 @@
 #!/bin/bash
 
-set -e
+# https://github.com/apache/hive/blob/master/packaging/src/docker/entrypoint.sh
 
-# Define variables
-METASTORE_SCHEMA_TOOL="$HIVE_HOME/bin/schematool"
+set +x
 
-# Check if the metastore schema exists
-if ! $METASTORE_SCHEMA_TOOL -info -dbType mysql -verbose 2>&1 | grep -q "Metastore schema version" ; then
-    echo "Initializing Hive Metastore..."
-    $METASTORE_SCHEMA_TOOL -initSchema -dbType mysql -verbose
-    echo "Hive Metastore initialized."
+: ${DB_DRIVER:=derby}
+
+[[ $VERBOSE = "true" ]] && VERBOSE_MODE="--verbose" || VERBOSE_MODE=""
+
+function initialize_hive {
+  set -x
+  COMMAND="-initOrUpgradeSchema"
+  if [ "$(echo "$HIVE_VER" | cut -d '.' -f1)" -lt "4" ]; then
+    COMMAND="-${SCHEMA_COMMAND:-initSchema}"
+  fi
+  if ! $HIVE_HOME/bin/schematool -info -dbType $DB_DRIVER -verbose 2>&1 | grep -iq "metastore schema version" ; then
+    $HIVE_HOME/bin/schematool -dbType $DB_DRIVER $COMMAND $VERBOSE_MODE
+    if [ $? -eq 0 ]; then
+      echo "Initialized schema successfully.."
+    else
+      echo "Schema initialization failed!"
+      exit 1
+    fi
+  fi
+}
+
+# handles schema initialization
+initialize_hive
+
+set +x
+export HIVE_CONF_DIR=$HIVE_HOME/conf
+if [ -d "${HIVE_CUSTOM_CONF_DIR:-}" ]; then
+  find "${HIVE_CUSTOM_CONF_DIR}" -type f -exec \
+    ln -sfn {} "${HIVE_CONF_DIR}"/ \;
+  export HADOOP_CONF_DIR=$HIVE_CONF_DIR
+  export TEZ_CONF_DIR=$HIVE_CONF_DIR
 fi
 
-# Start Hive Metastore Thrift Server and HiveServer2
-echo "Starting Hive Metastore Thrift Server..."
-$HIVE_HOME/bin/hive --service metastore &
+export HADOOP_CLIENT_OPTS="$HADOOP_CLIENT_OPTS -Xmx1G $SERVICE_OPTS"
 
-echo "Starting HiveServer2..."
-$HIVE_HOME/bin/hive --service hiveserver2 &
-
-# Keep the container running
-tail -f /dev/null
+if [ "${SERVICE_NAME}" == "hiveserver2" ]; then
+  export HADOOP_CLASSPATH=$TEZ_HOME/*:$TEZ_HOME/lib/*:$HADOOP_CLASSPATH
+  exec $HIVE_HOME/bin/hive --skiphadoopversion --skiphbasecp --service $SERVICE_NAME
+elif [ "${SERVICE_NAME}" == "metastore" ]; then
+  export METASTORE_PORT=${METASTORE_PORT:-9083}
+  exec $HIVE_HOME/bin/hive --skiphadoopversion --skiphbasecp $VERBOSE_MODE --service $SERVICE_NAME
+fi
